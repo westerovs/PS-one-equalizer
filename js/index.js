202942bf3a319b00e60787e4f720d3321775662a
Александр <westerovs@mail.ru>
2021-02-25 22:35:10 +0300

    fixed size controls list

--- js/index.js
+++ js/index.js
@@ -1,8 +1,14 @@
 /* eslint-disable */
+const trackList = document.querySelector('.track__list')
+const buttonTemplate = () => `<button type="button" class="track__item">1</button>`
 
-
-
-
+// const render = (container, template, place = `beforeend`) => {
+//     if (container instanceof Element) {
+//         container.insertAdjacentHTML(place, template);
+//     }
+// };
+//
+// render(trackList, buttonTemplate())
 
 
 
